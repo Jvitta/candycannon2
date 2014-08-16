343b950d5ad8a22791ad765441ef6a065ea6863c
Jack Vittimberga <jackvittimberga@Jacks-MacBook-Pro.local>
2014-08-15 17:47:50 -0700

    changed title

--- Choco.spritebuilder/Source/Menu.m
+++ Choco.spritebuilder/Source/Menu.m
@@ -10,20 +10,20 @@
 #import "CCPhysics+ObjectiveChipmunk.h"
 
 @implementation Menu{
-    CCNode *_cloud1;
+    /*CCNode *_cloud1;
     CCNode *_cloud2;
     CCNode *_cloud3;
     CCNode *_cloud4;
-    NSArray *_clouds;
+    NSArray *_clouds;*/
 }
 
 -(void)didLoadFromCCB{
-    _clouds = @[_cloud1,_cloud2,_cloud3,_cloud4];
+    //_clouds = @[_cloud1,_cloud2,_cloud3,_cloud4];
     self.userInteractionEnabled = YES;
 }
 
 -(void)update:(CCTime)delta{
-    _cloud1.position = ccp(_cloud1.position.x -0.50,_cloud1.position.y);
+    /*_cloud1.position = ccp(_cloud1.position.x -0.50,_cloud1.position.y);
     _cloud2.position = ccp(_cloud2.position.x -0.20,_cloud2.position.y);
     _cloud3.position = ccp(_cloud3.position.x -0.40,_cloud3.position.y);
     _cloud4.position = ccp(_cloud4.position.x -0.45,_cloud4.position.y);
@@ -32,7 +32,7 @@
         if(cloud.position.x < -200){
             cloud.position = ccp([[CCDirector sharedDirector] viewSize].width + 200,cloud.position.y);
         }
-    }
+    }*/
 }
 
 -(void)start{
